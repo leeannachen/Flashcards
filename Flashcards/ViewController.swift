32e52654c1b174e950850b0dad1047712257f2df
Lee <chen.leeanna@gmail.com>
2021-02-27 07:02:51 -0500

    first flashcard lab completed

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answer: UILabel!
+    @IBOutlet weak var question: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // How to get the borders?
+        question.layer.cornerRadius = 10
+        answer.layer.cornerRadius = 10
     }
-
-
+        
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        question.isHidden = true
+    }
+    
 }
 
