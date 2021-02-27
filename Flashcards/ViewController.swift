399711e7172856cf909f73f11cfd2f249761cd59
Lee <chen.leeanna@gmail.com>
2021-02-27 02:52:44 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Lee Anna Chen on 2/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
